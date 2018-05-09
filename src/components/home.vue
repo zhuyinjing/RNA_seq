24061739e70bb9776ec5f0039c60c140a087e749
zhuyinjing <zhuyinjing@seqsource.com>
2018-05-09 14:06:08 +0800

    add inital code

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import navbar from './navbar.vue'
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  components: {
+    navbar
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped="true">
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+      <el-menu-item index="1" @click="a">首页</el-menu-item>
+      <el-menu-item index="2">项目列表</el-menu-item>
+      <el-menu-item index="3">基因组浏览器</el-menu-item>
+      <el-menu-item index="4">关于</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: '1',
+    }
+  },
+  components: {
+  },
+  mounted () {
+  },
+  methods: {
+    a () {
+      this.axios.get('/get_file').then((res) => {
+        console.log(res.data)
+      })
+    },
+  }
+}
+</script>
+
+<style scoped="true">
+</style>
